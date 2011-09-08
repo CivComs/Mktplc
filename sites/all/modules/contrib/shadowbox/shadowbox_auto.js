67f601d6c796aa4aaf334b67dce01a3000762586
Alexey Iovchuk <iovchuk@buick.garage.bogus>
2011-09-08 12:06:53 +0300

    Shadowbox module added.

---
+++ sites/all/modules/contrib/shadowbox/shadowbox_auto.js
@@ -0,0 +1,21 @@
+(function ($) {
+
+Drupal.behaviors.shadowbox = {
+  attach: function(context, settings) {
+    if (settings.shadowbox.auto_enable_all_images == 1) {
+      $("a[href$='jpg'], a[href$='png'], a[href$='gif'], a[href$='jpeg'], a[href$='bmp'], a[href$='JPG'], a[href$='PNG'], a[href$='GIF'], a[href$='JPEG'], a[href$='BMP']").each(function() {
+        if ($(this).attr('rel') == '') {
+          if (settings.shadowbox.auto_gallery == 1) {
+            $(this).attr('rel', 'shadowbox[gallery]');
+          }
+          else {
+            $(this).attr('rel', 'shadowbox');
+          }
+        }
+      });
+    }
+    Shadowbox.setup();
+  }
+};
+
+})(jQuery);
