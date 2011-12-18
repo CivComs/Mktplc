39ad1924412d1a7b00fcbb11d127ab33ff835737
Alan Palazzolo <zzolo@147331.no-reply.drupal.org>
2011-12-18 13:51:12 -0800

    Removed code call to include google anayltics file, as the module handles all this.

--- sites/all/themes/civiccommons/template.php
+++ sites/all/themes/civiccommons/template.php
@@ -48,7 +48,6 @@ function civiccommons_preprocess_page(&$vars, $hook) {
   //drupal_add_js('https://getsatisfaction.com/civiccommons/topics.widget?callback=gsfnTopicsCallback&amp;style=idea', 'external', array('weight' => 100));
   drupal_add_js(drupal_get_path('theme', 'civiccommons') .'/js/getsatisfaction1.js', 'file');
   drupal_add_js(drupal_get_path('theme', 'civiccommons') .'/js/getsatisfaction2.js', 'file');
-  drupal_add_js(drupal_get_path('theme', 'civiccommons') .'/js/googleanalytics.js', 'file');
 }
 
 
