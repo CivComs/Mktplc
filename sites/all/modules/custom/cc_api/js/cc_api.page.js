9881bbe505fe04c19fc3b8c0a1e7f3da6a2252fc
Alan Palazzolo <zzolo@147331.no-reply.drupal.org>
2011-12-23 14:16:21 -0800

    Cleaning up a bit and adding more documentation.

--- sites/all/modules/custom/cc_api/js/cc_api.page.js
+++ sites/all/modules/custom/cc_api/js/cc_api.page.js
@@ -2,3 +2,23 @@
  * @file
  * Main JS file for the CC API documentation page.
  */
+
+// Namespace $ for jQuery
+(function($) {
+
+/**
+ * Drupal behavior for the documentation page.
+ */
+Drupal.behaviors.ccAPIDocPageTOC = {
+  attach: function(context, settings) {
+    $('body').once('cc-api-toc', function() {
+      $('.api-toc-container').tableOfContents($('.api-container'), {
+        topLinks: true,
+        depth: 5,
+        startLevel: 2
+      });
+    });
+  }
+};
+	
+})(jQuery);
