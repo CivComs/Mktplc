3de52391c30e5b878a15bc605e2d944f84c56d39
Alan Palazzolo <zzolo@147331.no-reply.drupal.org>
2012-02-20 12:12:50 -0600

    Cleaned up and add JS for CC theme #285.

--- sites/all/themes/civiccommons/js/civiccommons.js
+++ sites/all/themes/civiccommons/js/civiccommons.js
@@ -1,32 +1,33 @@
+/**
+ * @file
+ * Civic Common theme, general JS.
+ */
+ 
+// Namespace jQuery
+(function($) {
+  $(document).ready(function() {
+    // Fire of row evenizers on page load
+    evenizeRowHeights(".view-applications .views-row .views-field-field-application-sdesc");
+    evenizeRowHeights(".view-places .views-row .field-content");
+    evenizeRowHeights(".meta");
+    // Make front page columns same height
+    evenizeRowHeights(".front .region-content-main-left .view, .front .region-content-main-middle .view, .front .region-content-main-right .view");
+  });
 
-// Fire of row evenizers on page load
-jQuery(document).ready(evenizeRowHeights(".view-applications .views-row .views-field-field-application-sdesc"));
-jQuery(document).ready(evenizeRowHeights(".view-places .views-row .field-content"));
-jQuery(document).ready(evenizeRowHeights(".meta"));
-
-
-/*
-#
-#  makes all app listing rows the same height --
-#  using the height of the tallest row for everything.
-#
-*/
-function evenizeRowHeights(selector) {
-
-  return function() {
-  	var rowMaxHeight = 0;
-  	jQuery(selector).each(function () {
-  		 var elHeight = jQuery(this).height();
-  		 if(elHeight > rowMaxHeight){
-  			 rowMaxHeight = elHeight;
-  		 }
-  	});
-  		jQuery(selector).each(function () {
-				jQuery(this).height(rowMaxHeight);
-  	});
-  }  
-  	
-}
-
-
-
+  /**
+   * Makes all app listing rows the same height --
+   * using the height of the tallest row for everything.
+   */
+  function evenizeRowHeights(selector) {
+    var rowMaxHeight = 0;
+    $(selector).each(function () {
+      var elHeight = $(this).height();
+      if (elHeight > rowMaxHeight) {
+        rowMaxHeight = elHeight;
+      }
+    });
+    $(selector).each(function () {
+      $(this).height(rowMaxHeight);
+    });  
+  }
+})(jQuery);
